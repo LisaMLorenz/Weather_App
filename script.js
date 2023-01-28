5ba3203b68e6705b50ffca5727d59df9181e1a47
LisaMLorenz <hej@frau-lorenz.de>
2023-01-28 21:06:05 +0000

    buttons saved in local storage

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const buttonDiv = $("<div>").attr("id", "city-buttons-go"); // creating a div where I can put all the city buttons
 $('#history.list-group').append(buttonDiv); // appending the new ciyt button div to existing div in DOM
 var userInput = ('London');
+var userSearch = [];
+let storedCities = [];
 
 $("#search-button").click(function(event){ // and the search button is clicked
     event.preventDefault(); // stops page from reloading
@@ -13,10 +15,30 @@ $("#search-button").click(function(event){ // and the search button is clicked
     const cityButton = $("<button>").text(userInput).addClass("d-flex flex-column").css({"padding":'10px', "margin": '5px'}); // I want to create a button with that text content
     buttonDiv.append(cityButton); // button is appended to the button div
     $('#search-input').val(''); // and the text field is cleared
+    storedCities.push(userInput);
+    localStorage.setItem("Cities", JSON.stringify(storedCities));
 
     console.log(userInput); // prints the recent text field input in the console
-
+    console.log(storedCities);
+    
 });
+
+console.log(storedCities);
+
+$(document).ready(function(){
+    
+    storedCities = JSON.parse(localStorage.getItem("Cities"));
+if(storedCities){
+  for (let i = 0; i < storedCities.length; i++) {
+    let city = storedCities[i];
+    const cityButton = $("<button>").text(city).addClass("d-flex flex-column").css({"padding":'10px', "margin": '5px'});
+    buttonDiv.append(cityButton);
+  }
+}
+
+    console.log(storedCities);
+
+  });
 
 $('#clear-button').click(function(event){ // when the clear button is clicked
 
@@ -24,6 +46,8 @@ $('#clear-button').click(function(event){ // when the clear button is clicked
     console.log("Clear button clicked!"); // checks if clear button clicked
 
     $("#city-buttons-go").empty(); // the div that contains the city buttons is emptied
+    localStorage.clear();
+    storedCities = [];
 });
 
 
